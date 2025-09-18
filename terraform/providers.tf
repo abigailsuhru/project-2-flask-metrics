668cdc9bd2f68bbe7ea696294b60794ba2be97d2
AbigailSuhru <suhru.mini@gmail.com>
2025-09-18 18:01:37 +0530

    Debug on EKS stuff

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.12.1"
@@ -10,4 +18,29 @@ terraform {
 
 provider "aws" {
   region = var.region
+}
+
+# Fetch cluster details and authentication
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+# Configure Kubernetes provider
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# Configure Helm provider (uses Kubernetes provider settings)
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
 }
