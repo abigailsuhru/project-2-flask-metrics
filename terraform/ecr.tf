502792d7d10d578e75f640678446c5b54a9a10cd
AbigailSuhru <suhru.mini@gmail.com>
2025-09-18 19:08:57 +0530

    Debug repo is already created

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,12 @@
 resource "aws_ecr_repository" "app" {
+  name = "devops-project-2-app"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
   name                 = "${var.project_name}-app"
   image_scanning_configuration { scan_on_push = true }
   image_tag_mutability = "MUTABLE"
+
 }
