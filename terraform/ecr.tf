fb21a2f1e017b69b264163e1908896e40f30c4fc
AbigailSuhru <suhru.mini@gmail.com>
2025-09-18 19:11:08 +0530

    Debug duplicated line removed

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -4,9 +4,11 @@ resource "aws_ecr_repository" "app" {
   lifecycle {
     prevent_destroy = true
   }
-
-  name                 = "${var.project_name}-app"
-  image_scanning_configuration { scan_on_push = true }
+  
+  image_scanning_configuration 
+  {
+     scan_on_push = true
+  }
   image_tag_mutability = "MUTABLE"
 
 }
