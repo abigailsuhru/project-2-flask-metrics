91639644ca64e477e6fd76719ddd2519c62c24d6
AbigailSuhru <suhru.mini@gmail.com>
2025-09-21 01:20:16 +0530

    Aligning

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -31,16 +31,15 @@ data "aws_eks_cluster_auth" "cluster" {
 
 # Configure Kubernetes provider
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-# Configure Helm provider (uses Kubernetes provider settings)
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
