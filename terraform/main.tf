396862cf27c1e998f691916c7511a9b6fb26cb0d
AbigailSuhru <suhru.mini@gmail.com>
2025-09-24 14:50:13 +0530

    Removed missied Cloud connections

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,17 @@
 # Deploy the Helm chart for your app
 resource "helm_release" "myapp" {
   name       = "myapp"
-  namespace  = "default"
-  chart      = "./helm/myapp"
-  values     = [file("${path.module}/helm/myapp/values.yaml")]
+  namespace  = var.namespace
+  chart      = "../helm/myapp"
+  values     = [file("${path.module}/../helm/myapp/values.yaml")]
 
   # Optional: Set any additional values here
   set {
     name  = "image.repository"
-    value = "myapp-image-repo"
+    value = var.image_repository
   }
   set {
     name  = "image.tag"
-    value = "latest"
+    value = var.image_tag
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,3 @@
-output "region" { value = var.region }
-output "cluster_name" { value = module.eks.cluster_name }
-output "ecr_repo_url" { value = aws_ecr_repository.app.repository_url }
-output "gha_role_arn" { value = aws_iam_role.gha_deployer.arn }
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
