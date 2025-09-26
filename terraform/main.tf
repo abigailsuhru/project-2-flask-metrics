3a2881603d10357378c19f5c21ec7df27f38d88f
AbigailSuhru <suhru.mini@gmail.com>
2025-09-26 17:38:19 +0530

    Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 resource "helm_release" "my1app" {
   name      = "my1app"
   namespace = var.namespace
-  chart     = "../helm/my1app"
-  values    = [file("${path.module}/../helm/my1app/values.yaml")]
+  chart     = "../helm/myapp"
+  values    = [file("${path.module}/../helm/myapp/values.yaml")]
 
   set {
     name  = "image.repository"
