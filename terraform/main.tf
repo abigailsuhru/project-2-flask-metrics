da26558d6e4b1f38d434a7d15dc2de1e1731c16a
AbigailSuhru <suhru.mini@gmail.com>
2025-09-26 16:36:38 +0530

    Kubeconfig fix in main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,34 @@
-# Deploy the Helm chart for your app
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "my_ns" {
+  metadata {
+    name = var.namespace
+  }
+}
+
 resource "helm_release" "myapp" {
   name       = "myapp"
-  namespace  = var.namespace
+  namespace  = kubernetes_namespace.my_ns.metadata[0].name
   chart      = "../helm/myapp"
   values     = [file("${path.module}/../helm/myapp/values.yaml")]
 
-  # Optional: Set any additional values here
   set {
     name  = "image.repository"
     value = var.image_repository
   }
+
   set {
     name  = "image.tag"
     value = var.image_tag
   }
+
+  depends_on = [kubernetes_namespace.my_ns]
 }
